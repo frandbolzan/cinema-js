cb044f19ed03d70c0ec375ce8b12cdb0734c95a0
Francisco Daniel Bolzan Aramburu <78733848+frandbolzan@users.noreply.github.com>
2021-09-23 17:55:16 -0300

    v2.0

--- Js/form.js
+++ Js/form.js
@@ -1,22 +1,24 @@
-// Form
-const myForm = event => {
-    event.preventDefault()
-    const formState = []
-    let nombre = document.getElementById("nombre");
-    let año = document.getElementById("año");
-    let contador = 0;
+const form = document.getElementById('form')
+const movieContainer = document.getElementById('movies')
+let movieState = []
+let i = 0
 
-    !nombre.value || (!año.value || año.value <= 1900 || año.value >= 2022) ? alert('Ingresa un título y un año válidos.') :
-    formState.push({ element: 'nombre', value: nombre.value }, { element: 'año', value: año.value })
+const printMovieAndYear = movieState => {
+    const li = document.createElement('li')
 
-    const dato = document.getElementById('formValues')
-
-    const html = formState.map(item => {
-        let p = document.createElement('p')
-        p.innerText = item.value
-        dato.appendChild(p)
-    })
-
-    formState.length === 2 && contador++ && JSON.stringify(html); //Acá es donde me trabé, porque el contador nunca se acrecienta.
-
+    for(i; i < movieState.length; i++){
+        li.appendChild(document.createTextNode(`${movieState[i].movie} - ${movieState[i].year}`))
+        movieContainer.appendChild(li)
+    }
 }
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    const movieName = document.querySelector('#nombre').value
+    const movieYear = document.querySelector('#año').value
+    const message = document.querySelector('#message')
+
+    movieState.push({movie: movieName, year: movieYear})
+    movieState.length <= 5 && printMovieAndYear(movieState)
+    movieState.length === 5 && message.appendChild(document.createTextNode('¡Tenés muy buenos gustos!'))
+})
